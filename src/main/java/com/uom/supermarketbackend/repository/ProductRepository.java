89ec3f0ca604b57a55b4bbe17670a8fafcb340a0
vinodliyanage <vinodsliyanage@gmail.com>
2023-10-15 15:51:33 +0530

    change repositories from class to interfaces.

--- src/main/java/com/uom/supermarketbackend/repository/ProductRepository.java
+++ src/main/java/com/uom/supermarketbackend/repository/ProductRepository.java
@@ -1,7 +1,9 @@
 package com.uom.supermarketbackend.repository;
 
+import com.uom.supermarketbackend.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ProductRepository {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
--- src/main/java/com/uom/supermarketbackend/repository/UserRepository.java
+++ src/main/java/com/uom/supermarketbackend/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package com.uom.supermarketbackend.repository;
+import com.uom.supermarketbackend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public class UserRepository{
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
